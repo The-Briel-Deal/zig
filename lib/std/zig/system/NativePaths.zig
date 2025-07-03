b00261dbf18648516d283c41e2b0997a90867c6d
Gabriel Ford <gabe@fordltc.net>
2025-07-03 13:20:17 -0400

    WIP - prefer `-isystem` to `$C_INCLUDE_PATH` when searching for headers

--- lib/std/zig/system/NativePaths.zig
+++ lib/std/zig/system/NativePaths.zig
@@ -149,12 +149,12 @@ pub fn detect(arena: Allocator, native_target: *const std.Target) !NativePaths {
         // variables to search for headers and libraries.
         // We use os.getenv here since this part won't be executed on
         // windows, to get rid of unnecessary error handling.
-        if (std.posix.getenv("C_INCLUDE_PATH")) |c_include_path| {
-            var it = mem.tokenizeScalar(u8, c_include_path, ':');
-            while (it.next()) |dir| {
-                try self.addIncludeDir(dir);
-            }
-        }
+//        if (std.posix.getenv("C_INCLUDE_PATH")) |c_include_path| {
+//            var it = mem.tokenizeScalar(u8, c_include_path, ':');
+//            while (it.next()) |dir| {
+//                try self.addIncludeDir(dir);
+//            }
+//        }
 
         if (std.posix.getenv("CPLUS_INCLUDE_PATH")) |cplus_include_path| {
             var it = mem.tokenizeScalar(u8, cplus_include_path, ':');
